5f8d07e41dc1f7ede1402ebea7f366b6816a3f3a
webnetkz <8gg001@gmail.com>
2019-09-03 21:00:59 +0600

    append firsts styles and scripts for project

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
         <meta charset="utf-8">
         <title>index page</title>
         <!-- CSS Styles -->
-        <link rel="stylesheet" href="public/css/.css">
+        <link rel="stylesheet" href="public/css/indexStyles.css">
+        <link rel="stylesheet" href="public/css/mobileIndexStyles.css">
     </head>
     <body>
         
         <!-- JS Scripts -->
+        <script src="public/js/menu.js"></script>
     </body>
 </html>
