85e0150b5e882bc1089e759bf00aae94562d9f0a
webnetkz <8gg001@gmail.com>
2019-09-04 16:28:27 +0600

    append icons for header

--- index.php
+++ index.php
@@ -11,7 +11,14 @@
         <link rel="stylesheet" href="public/css/mobileIndexStyles.css">
     </head>
     <body>
-        
+        <header>
+            <div id="headerBlock">
+                <img src="public/img/icons/menu.svg" alt="menu bar" class="menuIcon">
+
+                <img src="public/img/icons/cart.svg" alt="market cart" class="searchIcon">
+                <img src="public/img/icons/search.svg" alt="search glass" class="searchIcon">
+            </div>
+        </header>
         <!-- JS Scripts -->
         <script src="public/js/menu.js"></script>
     </body>
