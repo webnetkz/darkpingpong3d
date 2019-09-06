b6bc441158bf57a9bf7778e61bdfb211a94d7618
webnetkz <8gg001@gmail.com>
2019-09-06 19:47:45 +0600

    update project to day

--- index.php
+++ index.php
@@ -4,20 +4,28 @@
 <!-- HTML Content -->
 <html>
     <head>
+        <!-- HTML Metas -->
         <meta charset="utf-8">
-        <title>index page</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Start page</title>
         <!-- CSS Styles -->
         <link rel="stylesheet" href="public/css/indexStyles.css">
         <link rel="stylesheet" href="public/css/mobileIndexStyles.css">
     </head>
     <body>
         <header>
+            <!-- Header Block -->
             <div id="headerBlock">
                 <img src="public/img/icons/menu.svg" alt="menu bar" class="menuIcon">
 
                 <img src="public/img/icons/cart.svg" alt="market cart" class="searchIcon">
                 <img src="public/img/icons/search.svg" alt="search glass" class="searchIcon">
             </div>
+            <!-- Index Content -->
+            <div id="indexContent">
+
+            </div>
+
         </header>
         <!-- JS Scripts -->
         <script src="public/js/menu.js"></script>
