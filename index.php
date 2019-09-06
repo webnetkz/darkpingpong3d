8722cae556685a2c91c5fe2316cd72b0aeebbadc
webnetkz <8gg001@gmail.com>
2019-09-06 21:06:07 +0600

    update index styles

--- index.php
+++ index.php
@@ -8,27 +8,32 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width,initial-scale=1.0">
         <title>Start page</title>
-        <!-- CSS Styles -->
+        <!-- CSS styles -->
         <link rel="stylesheet" href="public/css/indexStyles.css">
         <link rel="stylesheet" href="public/css/mobileIndexStyles.css">
     </head>
     <body>
         <header>
-            <!-- Header Block -->
+            <!-- Header block -->
             <div id="headerBlock">
                 <img src="public/img/icons/menu.svg" alt="menu bar" class="menuIcon">
 
                 <img src="public/img/icons/cart.svg" alt="market cart" class="searchIcon">
                 <img src="public/img/icons/search.svg" alt="search glass" class="searchIcon">
             </div>
-            <!-- Index Content -->
+            <!-- Index content -->
             <div id="indexContent">
                 <div class="indexContent">
-                    
+                    <h1 class="indexH1">Арендовать подходящее оборудование и инструменты. Прямо сейчас.</h1>
+
+                    <form action="#" class="indexForm">
+                        <input type="text" class="indexInp indexInpOne" placeholder="место нахождения">
+                        <input type="text" class="indexInp indexInpTwo" placeholder="поиск">
+                    </form>
                 </div>
             </div>
         </header>
-        <!-- JS Scripts -->
+        <!-- JS scripts -->
         <script src="public/js/menu.js"></script>
     </body>
 </html>
