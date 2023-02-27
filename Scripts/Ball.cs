59b1acc397cd0a0a62db41931b65acce6ad3314a
webnetkz <dbqqbq@gmail.com>
2023-02-27 21:02:35 +0600

    Append audio source of background, change color of ball

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ball : MonoBehaviour
 {
@@ -16,6 +17,15 @@ public class Ball : MonoBehaviour
 
     void Update()
     {
+      if(transform.position.z >= 25f || transform.position.z <= -4.3f) {
+        Invoke("Restart", 1f);
+      }
       //rb.AddForce(new Vector3(0, 0, 5f * Time.deltaTime), ForceMode.Impulse);
+    }
+
+
+    void Restart()
+    {
+      SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 }
